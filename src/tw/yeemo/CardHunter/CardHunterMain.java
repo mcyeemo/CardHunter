7cc35e82fec73ff8b61f6ad4e209fb15734c7e45
PangHu <i0987663350@gmail.com>
2018-07-12 21:34:01 +0800

    *新增OpenGUI功能

--- src/tw/yeemo/CardHunter/CardHunterMain.java
+++ src/tw/yeemo/CardHunter/CardHunterMain.java
@@ -1,13 +1,17 @@
 package tw.yeemo.CardHunter;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import tw.yeemo.CardHunter.events.OpenGUI;
 
 public class CardHunterMain extends JavaPlugin {
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         // Hello World
         getServer().getLogger().info("PANG_HU");
+
+        //Register Event
+        getServer().getPluginManager().registerEvents(new OpenGUI(),this);
 
     }
 }
---
+++ src/tw/yeemo/CardHunter/events/OpenGUI.java
@@ -0,0 +1,19 @@
+package tw.yeemo.CardHunter.events;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class OpenGUI implements Listener {
+
+    @EventHandler
+    public void OGUI(PlayerInteractEvent event){
+        if (event.getItem() == null && event.getItem().getType() != Material.BOOK ){
+            event.getPlayer().sendRawMessage("87");
+            return;
+        }else {
+            event.getPlayer().sendRawMessage("good");
+        }
+    }
+}
