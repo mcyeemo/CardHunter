bfbb3101cb02597ad4b17ec04f4b176fd64d6570
wenwen357951 <wenwen357951@gmail.com>
2018-07-12 16:04:54 +0800

    Add files via upload

---
+++ src/tw/yeemo/CardHunter/CardHunterMain.java
@@ -0,0 +1,11 @@
+package tw.yeemo.CardHunter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CardHunterMain extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        System.out.print("Hello World!");
+    }
+}
